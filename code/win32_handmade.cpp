18a226fb17ac094f1c8a104eb30210ec9f8a69dd
HusseinAzeez <hussein.azeez91@gmail.com>
2020-04-28 00:49:27 +0700

    Feat: implement direct sound & add sequare wave test

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdint.h>
 #include <xinput.h>
+#include <dsound.h>
 
 #define internal static
 #define global_variable static
@@ -34,6 +35,7 @@ struct win32WindowDimension
 // TODO(Xizors): This is global for now.
 global_variable bool running;
 global_variable win32OffscreenBuffer backBuffer;
+global_variable LPDIRECTSOUNDBUFFER secondaryBuffer;
 
 win32WindowDimension win32GetWindowDimension(HWND window)
 {
@@ -55,12 +57,12 @@ internal void renderGradient(win32OffscreenBuffer* buffer, int blueOffset, int g
 		for (int x = 0; x < buffer->width; x++)
 		{
 			/*
-			 * Pixel (32-bits)
-			 * LITILE ENDIAN ARCHITECTURE
-			 * Memory:      BB GG RR xx
-			 * Register:    xx RR GG BB
-			 * 0x xxRRGGBB
-			 */
+			* Pixel (32-bits)
+			* LITILE ENDIAN ARCHITECTURE
+			* Memory:      BB GG RR xx
+			* Register:    xx RR GG BB
+			* 0x xxRRGGBB
+			*/
 			uint8 blue = (x + blueOffset);
 			uint8 green = (y + greenOffset);
 			*pixel++ = ((green << 8) | blue);
@@ -98,14 +100,15 @@ internal void win32DisplayBufferInWindow(win32OffscreenBuffer* buffer,
 	int windowWidth, int windowHeight)
 {
 	/* StretchDIBits function copies the color data for a rectangle of pixels
-	 * in a DIB, JPEG, or PNG image to the specified destination rectangle.
-	 * If the destination rectangle is larger than the source rectangle,
-	 * this function stretches the rows and columns of color data
-	 * to fit the destination rectangle. If the destination rectangleis smaller
-	 * than the source rectangle,
-	 * this function compresses the rows and columns by using the specified raster operation.
-	 */
-	 // TODO(Xizors): Aspect ratio correction.
+	* in a DIB, JPEG, or PNG image to the specified destination rectangle.
+	* If the destination rectangle is larger than the source rectangle,
+	* this function stretches the rows and columns of color data
+	* to fit the destination rectangle. If the destination rectangleis smaller
+	* than the source rectangle,
+	* this function compresses the rows and columns
+	* by using the specified raster operation.
+	*/
+	// TODO(Xizors): Aspect ratio correction.
 	StretchDIBits(
 		deviceContext,
 		0, 0, windowWidth, windowHeight,
@@ -114,6 +117,67 @@ internal void win32DisplayBufferInWindow(win32OffscreenBuffer* buffer,
 		&buffer->info,
 		DIB_RGB_COLORS,
 		SRCCOPY);
+}
+
+internal void win32InitDSound(HWND window, int32 samplesPerSecond, int32 bufferSize)
+{
+	// NOTE(Xizors): Get a DirectSound object
+	// TODO(Xizors): Double-check that this works on XP - DirectSound8 or 7
+	LPDIRECTSOUND directSound;
+	if (SUCCEEDED(DirectSoundCreate(0, &directSound, 0)))
+	{
+		WAVEFORMATEX waveFormat = {};
+		waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+		waveFormat.nChannels = 2;
+		waveFormat.nSamplesPerSec = samplesPerSecond;
+		waveFormat.wBitsPerSample = 16;
+		waveFormat.nBlockAlign = (waveFormat.nChannels * waveFormat.wBitsPerSample) / 8;
+		waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+		waveFormat.cbSize = 0;
+
+		if (SUCCEEDED(directSound->SetCooperativeLevel(window, DSSCL_PRIORITY)))
+		{
+			// NOTE(Xizors): Create a primary buffer
+			DSBUFFERDESC bufferDescription = {};
+			bufferDescription.dwSize = sizeof(bufferDescription);
+			bufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+			LPDIRECTSOUNDBUFFER primaryBuffer;
+			if (SUCCEEDED(directSound->CreateSoundBuffer(&bufferDescription, &primaryBuffer, 0)))
+			{
+				HRESULT error = primaryBuffer->SetFormat(&waveFormat);
+				if (SUCCEEDED(error))
+				{
+					OutputDebugStringA("Primary buffer created successfully\n");
+				}
+				else
+				{
+					// TODO(Xizors): Diagnostic
+				}
+			}
+		}
+		else
+		{
+			// TODO(Xizors): Diagnostic
+		}
+		// NOTE(Xizors): Create a secondary buffer
+		DSBUFFERDESC bufferDescription = {};
+		bufferDescription.dwSize = sizeof(bufferDescription);
+		bufferDescription.dwFlags = 0;
+		bufferDescription.dwBufferBytes = bufferSize;
+		bufferDescription.lpwfxFormat = &waveFormat;
+
+		HRESULT error = directSound->CreateSoundBuffer(&bufferDescription, &secondaryBuffer, 0);
+		if (SUCCEEDED(error))
+		{
+			OutputDebugStringA("Secondary buffer created successfully\n");
+			// NOTE(Xizors): Start it playing!
+		}
+	}
+	else
+	{
+		// TODO(Xizors): Diagnostic
+	}
 }
 
 LRESULT CALLBACK win32MainWindowCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
@@ -249,8 +313,23 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, 
 			// get one device context and use it forever because we
 			// are not shearing it with anyone.
 			HDC deviceContext = GetDC(window);
+			// Display buffer variables
 			int xOffset = 0;
 			int yOffset = 0;
+			// DirectSound variables
+			int toneHz = 256; // ~Middle C HZ
+			int toneVolume = 3000;
+			int samplesPerSecond = 48000;
+			uint32 runningSampleIndex = 0;
+			int sequareWavePeriod = samplesPerSecond / toneHz;
+			int halfSequareWavePeriod = sequareWavePeriod / 2;
+			int bytesPerSample = sizeof(int16) * 2;
+			int secondaryBufferSize = samplesPerSecond * bytesPerSample;
+
+			// Init Direct Sound
+			win32InitDSound(window, samplesPerSecond, secondaryBufferSize);
+			secondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
+
 			running = true;
 			while (running)
 			{
@@ -301,6 +380,57 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, 
 					}
 				}
 				renderGradient(&backBuffer, xOffset, yOffset);
+				// TODO(Xizors): DirectSound output test
+				// TODO(Xizors): More test here!.
+				DWORD playCursor;
+				DWORD writeCursor;
+				if (SUCCEEDED(secondaryBuffer->GetCurrentPosition(&playCursor, &writeCursor)))
+				{
+					DWORD bytesToWrite;
+					DWORD bytesToLock = runningSampleIndex * bytesPerSample % secondaryBufferSize;
+					if (bytesToLock > playCursor)
+					{
+						bytesToWrite = (secondaryBufferSize - bytesToLock);
+						bytesToWrite += playCursor;
+					}
+					else
+					{
+						bytesToWrite = playCursor - bytesToLock;
+					}
+					/*
+					* [int16 int16] [int16 int16] [int16 int16] [int16 int16] ...
+					* [LEFT  RIGHT] [LEFT  RIGHT] [LEFT  RIGHT] [LEFT  RIGHT] ...
+					*/
+					void* region1;
+					DWORD region1Size;
+					void* region2;
+					DWORD region2Size;
+					if (SUCCEEDED(secondaryBuffer->Lock(bytesToLock, bytesToWrite,
+						&region1, &region1Size,
+						&region2, &region2Size,
+						0)))
+					{
+						// TODO(Xizors): Assert that region1Size/region2Size is valid
+						int16* sampleOut = (int16*)region1;
+						DWORD region1SampleCount = region1Size / bytesPerSample;
+						DWORD region2SampleCount = region2Size / bytesPerSample;
+
+						for (DWORD sampleIndex = 0; sampleIndex < region1SampleCount; sampleIndex++)
+						{
+							int16 sampleValue = ((runningSampleIndex++ / halfSequareWavePeriod) % 2) ? toneVolume : -toneVolume;
+							*sampleOut++ = sampleValue;
+							*sampleOut++ = sampleValue;
+						}
+
+						for (DWORD sampleIndex = 0; sampleIndex < region2SampleCount; sampleIndex++)
+						{
+							int16 sampleValue = ((runningSampleIndex++ / halfSequareWavePeriod) % 2) ? toneVolume : -toneVolume;
+							*sampleOut++ = sampleValue;
+							*sampleOut++ = sampleValue;
+						}
+						secondaryBuffer->Unlock(region1, region1Size, region2, region2Size);
+					}
+				}
 				win32WindowDimension dimension = win32GetWindowDimension(window);
 				win32DisplayBufferInWindow(&backBuffer, deviceContext,
 					dimension.width, dimension.height);
